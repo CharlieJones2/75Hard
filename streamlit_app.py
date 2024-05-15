78adf8bcb880d02ddb2662b030e05270ac000d98
Charlie Jones <125832247+CharlieJones2@users.noreply.github.com>
2024-05-15 07:15:03 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
 import streamlit as st
-import datetime as dt
+from datetime import date
 
 # Function to display the tracker for one participant
 def participant_tracker():
     #for day in range(1, 76):
-    st.write(f"Day {dt._Date.today()}:")
-    st.checkbox(f"Follow a diet (Whole foods, healthy meals ONLY)", key=f"rule1_{dt._Date.today()}")
-    st.checkbox(f"Drink 1 gallon (3.7 liters) of water", key=f"rule2_{dt._Date.today()}")
-    st.checkbox(f"First 45 minute exercise", key=f"rule3_{dt._Date.today()}")
-    st.checkbox(f"Second 45 minute exercise", key=f"rule3_{dt._Date.today()}")
-    st.checkbox(f"Read 10 pages of a book", key=f"rule4_{dt._Date.today()}")
-    st.checkbox(f"Take a progress picture", key=f"rule5_{dt._Date.today()}")
+    st.write(f"Day {date.today()}:")
+    st.checkbox(f"Follow a diet (Whole foods, healthy meals ONLY)", key=f"rule1_{date.today()}")
+    st.checkbox(f"Drink 1 gallon (3.7 liters) of water", key=f"rule2_{date.today()}")
+    st.checkbox(f"First 45 minute exercise", key=f"rule3_{date.today()}")
+    st.checkbox(f"Second 45 minute exercise", key=f"rule4_{date.today()}")
+    st.checkbox(f"Read 10 pages of a book", key=f"rule5_{date.today()}")
+    st.checkbox(f"Take a progress picture", key=f"rule6_{date.today()}")
     st.write("---")
 
 # Main function to display the tracker for both participants
